b1a94a7406151774cdc423805d26713c462f8b84
nabizadeh-m <mahrokh.nabizadeh4@gmail.com>
2025-01-25 14:01:11 +0330

    Updates

--- assets/resume-D0GZlGQT.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const e=document.querySelector(".resumeContainer");html2pdf().set({margin:[0,-2,0,-35],filename:"Nabizadeh_CV.pdf",html2canvas:{scale:1},jsPDF:{unit:"mm",format:"a4",orientation:"portrait",no_copy:!1}}).from(e).save();
---
+++ assets/resume-qFGo7JTe.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";const e=document.querySelector(".resumeContainer");html2pdf().set({margin:[-1,-2,-4,10],filename:"Nabizadeh_CV.pdf",html2canvas:{scale:1},jsPDF:{unit:"mm",format:"a4",orientation:"portrait",no_copy:!1}}).from(e).save();
